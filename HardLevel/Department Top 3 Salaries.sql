ae6c96e91e4bb87173f115760711b0e7321543d5
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-08-25 17:36:09 -0700

    Create Department Top 3 Salaries.sql

---
+++ HardLevel/Department Top 3 Salaries.sql
@@ -0,0 +1,10 @@
+WITH CTE AS (
+  SELECT DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS rnk,
+d.name AS Department, e.name AS Employee , e.salary AS Salary 
+FROM Employee e
+JOIN Department d 
+ON e.departmentid = d.id
+)
+SELECT Department, Employee, Salary
+FROM CTE
+WHERE rnk < 4
