a3d9b0631818ca2218d343656b29443500023ecc
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-10-11 15:56:38 -0700

    Update Gegraphy Report.sql

--- codingNinjas/Gegraphy Report.sql
+++ codingNinjas/Gegraphy Report.sql
@@ -23,3 +23,10 @@ FROM
 CTE c 
 FULL JOIN CTE1 c1 ON c.id = c1.id
 FULL JOIN CTE2 c2 ON c1.id = c2.id
+
+-------------------------------------------------------------------------
+SELECT MAX(CASE WHEN continent = 'America' THEN name END )AS America, 
+MAX(CASE WHEN continent = 'Asia' THEN name END )AS Asia, 
+MAX(CASE WHEN continent = 'Europe' THEN name END )AS Europe 
+FROM (SELECT *, ROW_NUMBER()OVER(PARTITION BY continent ORDER BY name) AS row_id
+ FROM student) AS t GROUP BY row_id
