f520073cfb3b34a563c159d5adc1f2c60ee9d1c7
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-10-25 16:54:25 -0700

    Update Activity Participants.sql

--- codingNinjas/Activity Participants.sql
+++ codingNinjas/Activity Participants.sql
@@ -2,22 +2,9 @@ WITH CTE AS(
     SELECT COUNT(id) as cnt, activity
     FROM Friends
     GROUP BY activity
-),
-CTE1 AS(
-    SELECT activity
-    FROM CTE
-    ORDER BY cnt DESC
-    LIMIT 1
-),
-CTE2 AS(
-    SELECT activity
-    FROM CTE
-    ORDER BY cnt ASC
-    LIMIT 1
 )
-
-SELECT f.activity
-FROM Friends f 
-WHERE activity NOT IN (SELECT activity FROM CTE1)
-AND activity NOT IN(SELECT activity FROM CTE2)
+SELECT activity
+FROM Friends  
 GROUP BY activity
+HAVING COUNT(id) < (SELECT MAX(cnt) FROM CTE)
+AND COUNT(id) > (SELECT MIN(cnt) FROM CTE)
