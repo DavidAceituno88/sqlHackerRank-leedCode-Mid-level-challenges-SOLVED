36943d05d17961cde4f25009c68175582eb4a851
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2023-11-27 17:28:53 -0800

    Update confirmation signups.sql

--- LEETCODE/confirmation signups.sql
+++ LEETCODE/confirmation signups.sql
@@ -6,3 +6,11 @@ FROM confirmations c
 RIGHT JOIN signups s
 ON c.user_id = s.user_id
 GROuP BY s.user_id
+
+SELECT s.user_id, 
+ROUND(AVG(CASE WHEN action_value 
+= 'confirmed' THEN 1.00 ELSE 0.00 END),2) 
+AS confirmation_rate FROM Signups s 
+LEFT JOIN Confirmations c 
+ON s.user_id = c.user_id 
+GROUP BY s.user_id
